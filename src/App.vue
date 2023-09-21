fd3b8a1acfaf5dd941955f5bd9adc6657984aa6c
AntonioLabinjan <antoniolabinjan5@gmail.com>
2023-09-21 12:17:15 +0200

    Prvi commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <ImageUploader />
+    <ImageGallery />
+  </div>
+</template>
+
+<script>
+import ImageUploader from "./components/ImageUploader.vue"; 
+import ImageGallery from "./components/ImageGallery.vue"; 
+
+export default {
+  name: 'App',
+  components: {
+    ImageUploader, 
+    ImageGallery, 
+  }
+}
+</script>
+<style src="./assets/styles/styles.css"></style>
---
+++ src/components/ImageGallery.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div v-for="(image, index) in images" :key="index">
+      <img :src="image" alt="Slika">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      images: [] 
+    };
+  }
+};
+</script>
